42422eadd84454012726b275f1aebfe0991bb435
Makhruf <161798155+Makhruf@users.noreply.github.com>
2024-03-18 11:34:34 +0700

    Code pertemuan 1

---
+++ index.js
@@ -0,0 +1,65 @@
+const express = require('express');
+const app = express();
+const port = 5000;
+
+
+//pertemuan 2
+app.get('/', (req, res) => {
+    res.send(`Hello Express!`)
+});
+
+app.get('/',(req, res)=> {
+    res.send('Get-Home Page')
+});
+
+//pertemuan 3
+app.get('/mahasiswa',(req, res)=>{
+
+     res.json(mahasiswa)
+});
+
+
+app.get('/get-mahasiswa-by-nim', (req, res) =>{
+    const nim = req.query.nim;
+
+    res.send(`Mahasiswa dengan nim : ${nim} ditemukan`)
+});
+
+app.get('/nilai-persemester',(req, res) => {
+    const nim = req.query.nim;
+    const semester = req.query.semester;
+
+    res.send(`Nilai mahasiswa nim : ${nim} semester ${semester} ditemukan`)
+})
+
+// req.params dengan 1 parameter
+app.get('/mahasiswa/:nim', (req, res) => {
+    const nim = req.params.nim;
+
+    res.send(`Mahasiswa dengan nim : ${nim} ditemukan` )
+});
+
+// req.params dengan 2 parameter
+app.get('/nilai/:nim/:semester',(req, res)=> {
+    const nim = req.params.nim;
+    const semester = req.params.semester;
+
+    res.send(`Nilai mahasiswa nim : ${nim} semester ${semester} ditemukan`)
+});
+
+//pertemuan 2
+app.post('/', (req, res) =>{
+    res.send(`Post Data`)
+});
+
+app.put('/', (req, res) =>{
+    res.send(`update data success`) 
+});
+
+app.delete('/', (req, res) =>{
+    res.send(`Hapus Data Berhasil`)
+});
+
+app.listen(port, ()=> {
+    console.log(`server berjalan dengan localhost:${port}`)
+});
